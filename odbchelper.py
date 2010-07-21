8365d76bc6f11ffd553b648940d6b2c88a9da025
David Webb <gerberduffy@gmail.com>
2010-07-20 18:52:32 -0700

    Removed accidentally added python files.

--- odbchelper.py
+++
@@ -1,9 +0,0 @@
-def buildConnectionString(params) :
-	return ";".join(["%s=$s" % (k,v) for k, v in params.items()])
-
-if __name__ == "__main__" :
-	myparams = {"server" : "mpilgrim", \
-		    "database" : "master", \
-		    "uid" : "sa", \
-		    "pwd" : "secret" }
-	print buildConnectionString(myParams)
